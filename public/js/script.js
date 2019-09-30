9fa9f4e18685d525bfbe275f8f5efa841264776f
ferchuqui <luisfernandofarji@hotmail.com>
2019-09-30 17:29:28 -0300

    Added fetch to Backend

--- public/js/script.js
+++ public/js/script.js
@@ -15,10 +15,19 @@
       button.disabled = false
     }
   }
+  const translate = function(){
+    fetch('/translate',{
+      method: "POST",
+      body: JSON.stringify({
+        text: original.value 
+      })
+    })
+  }
   original.addEventListener('keyup', validate )
   selectOrigin.addEventListener('change', validate)
   selectTarget.addEventListener('change', validate)
   validate()
+  button.addEventListener('click', translate)
 
 })()
 
